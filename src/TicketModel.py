37b0acf0d094ea3b0401f482fae95662468d7c8e
Sean R. Spillane <seans@cs.albany.edu>
2012-10-06 14:34:45 -0400

    Added some functions to Ticket.
    I added status flags and several methods to make merging and copying
    Tickets easy. Now, the Controllers can simply reify Tickets, copy them
    with edits, and merge the results, then tell index to update. Thus, we
    can capture most of the easy commands with a simple workflow.

--- src/TicketModel.py
+++ src/TicketModel.py
@@ -21,14 +21,27 @@ import hashlib
 class TicketModel:
     def __init__(self, 
             title, 
-            summary=None, 
             author=None,
-            creation_date=datetime.today()
+            summary=None, 
+            ticket_open=True,
+            working=False,
+            creation_date=datetime.today(),
+            ticket=None
             ):
+        if not ticket is None:
+            self.__title = ticket.getTitle() if not title is None
+            self.__author = ticket.getAuthor() if not author is None
+            self.__summary = ticket.getSummary() if not summary is None
+            self.__ticket_open = ticket.isOpen() and ticket_open
+            self.__working = ticket.isWorking() or working
+            self.__creation_date = ticket.getSummary() if not creation_date is None
+            pass
         self.__title = title
-        self.__summary = summary
         self.__author = author
+        self.__summary = summary
         self.__creation_date = creation_date
+        self.__open = ticket_open
+        self.__working = working
         pass
 
     def getTitle(Self):
@@ -44,6 +57,33 @@ class TicketModel:
         return self.__creation_date
 
     def getID(self):
-        return hashlib.sha256(self.__title.encode('utf8')).digest()
+        return getID(self.__title)
+
+    def isWorking(self):
+        return self.__working
+
+    def isOpen(self):
+        return self.__open
+
+    def merge(self, other):
+        '''
+        Given another Ticket, return a new Ticket, derived from our Ticket,
+        overlaid with the other Ticket.
+        '''
+        return Ticket(ticket=self,
+                title=other.getTitle(),
+                author=other.getAuthor(),
+                summary=other.getSummary(),
+                ticket_open=other.isOpen(),
+                working=other.isWorking(),
+                creation_date=other.creation_date())
+        pass
+
     pass
+
+def getID(unicode_string):
+    '''
+    Given a unicode string, compute the SHA256 hexdigest.
+    '''
+    return hashlib.sha256(self.__title.encode('utf8')).digest()
 
