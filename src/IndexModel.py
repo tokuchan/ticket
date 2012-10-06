21016159f6dc264f09367ae8c4e13752aede9e2f
Sean R. Spillane <seans@cs.albany.edu>
2012-10-06 14:36:30 -0400

    Added some comments.
    I thought about how some of the methods could work. I wrote these
    thoughts as comments. When I can get the network back, I'll look up how
    to serialize and de-serialize objects in Python. It should be pretty
    simple. Once I have read and write object methods, the rest should be
    pretty simple. We can determine if the index has a ticket by trying to
    get that ticket's ID's object. At the same point, that's also how we can
    try to reify it. Only if that fails, in some cases, will we then try to
    use the database to reify.

--- src/IndexModel.py
+++ src/IndexModel.py
@@ -18,21 +18,47 @@ class IndexModel:
         Extract the ID of an extant Ticket, then update our copy of that
         ticket. If the ID does not obtain, then we create our ticket first.
         '''
+        # The ticket's ID.
+        # Reify the Ticket, using reifyTicket().
+        # Merge the two tickets into one.
+        # Write the new ticket object to the index.
         pass
 
     def reifyTicket(self, ticket_id):
         '''
         Given a ticket ID, create a new Ticket object, and fill it in with the
-        details from our index. If there is now ticket in our index, we should
+        details from our index. If there is no ticket in our index, we should
         then try to get a reified Ticket from the main database, and return
         that.
         '''
+        # Does ticket ID exist in index?
+        # If not, does ticket ID exist in database?
+        # If so, retrieve ticket data from database.
+        # If so, retrieve ticket data from index.
+        # Given ticket data, construct and return a new Ticket.
         pass
 
     def deleteTicket(self, ticket_id):
         '''
         Given a ticket id, remove that Ticket from our index. If there is no
         Ticket in our index, then simply do nothing.
+        '''
+        # The ticket's ID.
+        # Does the ticket exist in our index?
+        # If so, delete it.
+        pass
+
+    def __write_object(self, objID, objRef):
+        '''
+        Given an object reference, use serialization to record that object to a
+        file in the index folder, named by the objID.
+        '''
+        pass
+
+    def __read_object(self, objID):
+        '''
+        Given an object ID, use serialization to read in that object from a
+        file in the index, using the object ID.
         '''
         pass
     pass
